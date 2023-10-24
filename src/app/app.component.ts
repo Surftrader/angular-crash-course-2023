167e6a770a0879f0b8b660cdb0d346c700ecac3c
Sergiy Poddubka <poseal@gmail.com>
2023-10-25 01:16:48 +0300

    create product (post)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,24 +15,23 @@ export class AppComponent implements OnInit {
   // products: IProduct[] = data
   // products: IProduct[] = []
   loading = false
-  products$: Observable<IProduct[]>
+  // products$: Observable<IProduct[]>
   term = ''
 
   constructor(
-    private productsService: ProductsService,
+    public productsService: ProductsService,
     public modalService: ModalService
   ) {
   }
 
   ngOnInit(): void {
     this.loading = true
-    this.products$ = this.productsService.getAll().pipe(
-      tap(() => this.loading = false)
-    )
+    // this.products$ = this.productsService.getAll().pipe(
+    //   tap(() => this.loading = false)
+    // )
 
-    // this.productsService.getAll().subscribe(products => {
-    //   this.products = products
-    //   this.loading = false
-    // })
+    this.productsService.getAll().subscribe(() => {
+      this.loading = false
+    })
   }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http'
-import { catchError, delay, Observable, retry, throwError } from 'rxjs'
+import { catchError, delay, Observable, retry, throwError, tap } from 'rxjs'
 import { IProduct } from '../models/product'
 import { ErrorService } from './error.service'
 
@@ -13,6 +13,9 @@ export class ProductsService {
     private errorService: ErrorService
   ) {
   }
+
+  products: IProduct[] = []
+
   getAll(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
       // params: new HttpParams().append('limit', 5) or
@@ -23,8 +26,16 @@ export class ProductsService {
     }).pipe(
       delay(200),
       retry(2),
+      tap(products => this.products = products),
       catchError(this.errorHandler.bind(this))
     )
+  }
+
+  create(product: IProduct): Observable<IProduct> {
+    return this.http.post<IProduct>('https://fakestoreapi.com/products', product)
+      .pipe(
+        tap(prod => this.products.push(prod))
+      )
   }
 
   private errorHandler(error: HttpErrorResponse) {
