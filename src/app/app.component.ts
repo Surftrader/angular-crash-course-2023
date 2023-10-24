5dffde2bb4671e67e2a0594aa80521c0a8ae87ec
Sergiy Poddubka <poseal@gmail.com>
2023-10-25 01:29:32 +0300

    add some routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/product'
-// import { products as data } from './data/products'
-import { ProductsService } from './services/products.service';
-import { tap, Observable } from 'rxjs'
-import { ModalService } from './services/modal.service';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'angular app';
-  // products: IProduct[] = data
-  // products: IProduct[] = []
-  loading = false
-  // products$: Observable<IProduct[]>
-  term = ''
-
-  constructor(
-    public productsService: ProductsService,
-    public modalService: ModalService
-  ) {
-  }
-
-  ngOnInit(): void {
-    this.loading = true
-    // this.products$ = this.productsService.getAll().pipe(
-    //   tap(() => this.loading = false)
-    // )
-
-    this.productsService.getAll().subscribe(() => {
-      this.loading = false
-    })
-  }
+export class AppComponent {
 }
